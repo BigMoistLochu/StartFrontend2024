eb0c778cd632fc2bfe075dc2df649326ca1ff3e0
BigMoistLochu <krok.konrad1@gmail.com>
2024-10-04 03:34:07 +0200

    added npm stuff, create new project for training

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,17 +1,32 @@
-console.log("hey");
+// Importowanie modułu jsonwebtoken
+// const jwt = require('jsonwebtoken');
 
-document.getElementById('fetch-button').addEventListener('click', () => {
-    fetch('http://localhost:8080/hello')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(data => {
-            console.log(data)
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-        });
-});
+// console.log(jwt.decode("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"));
+
+// const buttonel = document.getElementById('fetch-button').addEventListener('click', () => {
+//     fetch('http://localhost:8080/hello')
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//             return response.text();
+//         })
+//         .then(data => {
+//             console.log(data)
+//         })
+//         .catch(error => {
+//             console.error('There was a problem with the fetch operation:', error);
+//         });
+// });
+
+
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+  }
+  
+  function processUserInput(callback) {
+    const name = "Alice"; // W tym przykładzie po prostu hardcodujemy wartość
+    callback(name); // Wywołujemy callback z argumentem
+  }
+  
+  processUserInput(greet); // Wypisze: Hello, Alice!
